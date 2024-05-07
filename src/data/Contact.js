c49449d65a8008d25afd6e61c368e1527406de3c
Sean Quiambao <91030482+seanquiambao@users.noreply.github.com>
2024-05-07 16:13:20 -0700

    Update Contact.js

--- src/data/Contact.js
+++ src/data/Contact.js
@@ -20,6 +20,6 @@ export const CONTACT = [
   },
   {
     icon: <IoDocumentText className={CSS} />,
-    link: "https://drive.google.com/file/d/1yaR0pxpPlE6xT_UKB5dgPMRO0d99R3jz/view?usp=sharing",
+    link: "https://drive.google.com/file/d/1nClFUJK-3DSWQGrbGWW06MiCCkNvhDyh/view?usp=sharing",
   },
 ];
